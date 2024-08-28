6d96394816f5d8fda1f40a9f785cf1fdcd07ec59
Ricardo Ferreira <ricardo.adm115@gmail.com>
2024-08-28 14:57:49 -0300

    Salvando mudanças antes de reescrever o histórico

--- scripts.js
+++ scripts.js
@@ -3,14 +3,9 @@ function generateNuber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    if(min >= max){
-        alert("O valor minimo tem que ser MENOR que o valor máximo")
+    const result = Math.floor(Math.random() * ([max - min + 1]) + min)
 
-    } else {
-      const result = Math.floor(Math.random() * ([max - min + 1]) + min)
-
-      alert(result)
-    } 
+    alert(result)
 
 }
 
