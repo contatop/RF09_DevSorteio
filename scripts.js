c4ede480a8af73cc689e77cac990959f1864a27e
Ricardo Ferreira <ricardo.adm115@gmail.com>
2024-08-28 15:15:45 -0300

    Atualizando js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,14 @@ function generateNuber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
-    const result = Math.floor(Math.random() * ([max - min + 1]) + min)
+    if(min >= max){
+        alert("O valor minimo tem que ser MENOR que o valor máximo")
 
-    alert(result)
+    } else {
+      const result = Math.floor(Math.random() * ([max - min + 1]) + min)
+
+      alert(result)
+    } 
 
 }
 
